f6a9f35110ff04d84c1ea6d8afd2978c483922bf
ImakeWebsitesForme <sterlin.jaden8@gmail.com>
2025-01-30 15:34:36 -0500

    otaku

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.getElementById("animeButton").addEventListener("click", function() {
     const facts = [
-        "Did you know that **Naruto**'s creator, Masashi Kishimoto, originally wanted Naruto to be a comic about a ninja who didn't know any jutsu?",
+        "Did you know that **Naruto**'s! creator, Masashi Kishimoto, originally wanted Naruto to be a comic about a ninja who didn't know any jutsu?",
         "In Japan, it's common to see anime-inspired **bento** lunches, with cute characters made out of rice and vegetables!",
         "The term **otaku** originally meant a person with an obsession, but in Japan, it's now commonly used to describe passionate anime fans.",
         "**Shounen** anime like **One Piece** and **Dragon Ball Z** have become global phenomenons, attracting fans from all around the world!",
